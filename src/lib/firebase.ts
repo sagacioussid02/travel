443c84dfc0bcc5a8df3d2ea67988ba0e16139327
Siddharth Shankar <siddharthuncc@gmail.com>
2025-07-28 04:43:30 +0000

    Shouldn't it should use binosusai.com which is in comicscript project?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOvifi-9GbjLYRgunC6ZWaC-sGQTD2xcM",
-  authDomain: "sidoni-ai.firebaseapp.com",
-  projectId: "sidoni-ai",
-  storageBucket: "sidoni-ai.appspot.com",
+  authDomain: "binosusai.com",
+  projectId: "binosusai",
+  storageBucket: "binosusai.appspot.com",
   messagingSenderId: "1078070784045",
   appId: "1:1078070784045:web:cbb2a74e075e96317148a4"
 };
