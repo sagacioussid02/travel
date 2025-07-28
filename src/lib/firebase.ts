c41ade7dfebc5611aa46b3f748b27a0781125c30
Siddharth Shankar <siddharthuncc@gmail.com>
2025-07-28 04:54:16 +0000

    Your Firebase Project ID is: ComicScript

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOvifi-9GbjLYRgunC6ZWaC-sGQTD2xcM",
-  authDomain: "binosusai.firebaseapp.com",
-  projectId: "binosusai",
-  storageBucket: "binosusai.appspot.com",
+  authDomain: "ComicScript.firebaseapp.com",
+  projectId: "ComicScript",
+  storageBucket: "ComicScript.appspot.com",
   messagingSenderId: "1078070784045",
   appId: "1:1078070784045:web:cbb2a74e075e96317148a4"
 };
