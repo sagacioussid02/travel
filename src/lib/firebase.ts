8c1c91cc83f0d65894a1d13f98437d123c39656d
Siddharth Shankar <siddharthuncc@gmail.com>
2025-07-28 02:07:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,14 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import 'dotenv/config'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBOvifi-9GbjLYRgunC6ZWaC-sGQTD2xcM",
-    authDomain: "sidoni-ai.firebaseapp.com",
-    projectId: "sidoni-ai",
-    storageBucket: "sidoni-ai.firebasestorage.app",
-    messagingSenderId: "1078070784045",
-    appId: "1:1078070784045:web:cbb2a74e075e96317148a4",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
