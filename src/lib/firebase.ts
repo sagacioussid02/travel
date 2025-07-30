358765f783052df70320799590fc6811d437b0ce
Siddharth Shankar <siddharthuncc@gmail.com>
2025-07-30 19:47:09 +0000

    The API is publicly accessible. I pushed the code in public github. It h

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyM6DPHM1mLrPqPVXR47KYfc_D_RqGTF4",
-  authDomain: "comicscript-e26wr.firebaseapp.com",
-  projectId: "comicscript-e26wr",
-  storageBucket: "comicscript-e26wr.appspot.com",
-  messagingSenderId: "545455533059",
-  appId: "1:545455533059:web:869bfc3b43fd5e02a03236"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
